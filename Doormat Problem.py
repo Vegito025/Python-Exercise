6211e6f74f8af5fd7fc84276d3a1f054e7063082
Vegito025 <89723942+Vegito025@users.noreply.github.com>
2021-09-18 11:03:51 +0530

    Create Doormat Problem.py

---
+++ Doormat Problem.py
@@ -0,0 +1,7 @@
+m, n = map(int, input().split())
+
+for i in range(1, m, 2):
+    print((i* ".|.").center(n, "-"))
+print("WELCOME".center(n,"-"))
+for i in range(m-2, -1, -2):
+    print((i* ".|.").center(n, "-"))
